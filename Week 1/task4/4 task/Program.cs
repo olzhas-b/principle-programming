c3dc890db5a3c1179f0bbe10b2405780db9b2c32
qwerty321kz <oljas.bazarbekov15@gmail.com>
2019-02-15 19:18:31 +0600

    first commit

---
+++ Week 1/task4/4 task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //k is support variable
+            int k = 0;
+            // n is coloum and row
+            int n = int.Parse(Console.ReadLine());
+            int [,]a = new int[n,n];
+            // create massive
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    // k is equal 0 and evey time +2 is that mean algorithm to paint picture low than diagonal
+                    if (i + j > k)
+                        continue;
+                    else
+                        Console.Write("[*]");
+                }
+                k += 2;
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
