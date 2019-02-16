f4f7cc4c517da96a2b160e8078ac6fe781744d8a
qwerty321kz <oljas.bazarbekov15@gmail.com>
2019-02-16 09:42:59 +0600

    changed

---
+++ Week 2/task 3/task 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\x");
+            PrintInfo(dir, 0);
+        }
+
+        static void PrintInfo(FileSystemInfo f, int k)
+        {
+            string s = new string(' ', k);
+            Console.WriteLine(s + f.Name);
+
+            if (f.GetType() == typeof(DirectoryInfo))
+            {
+                FileSystemInfo[] arr = ((DirectoryInfo)f).GetFileSystemInfos();
+                for (int i = 0; i < arr.Length; ++i)
+                {
+                    PrintInfo(arr[i], k + 3);
+                }
+            }
+
+        }
+    }
+}
--- Week 2/task 3/task 3/Properties/AssemblyInfo.cs
+++ Week 2/task 3/task 3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("task3")]
+[assembly: AssemblyTitle("task 3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("task3")]
+[assembly: AssemblyProduct("task 3")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("dccb2827-cda9-47c8-940d-d7506542ef38")]
+[assembly: Guid("ec10b28d-e60a-463a-9556-f18168ee5c97")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
